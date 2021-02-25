e8935d37001b7123b824789b18a0634aed603dc4
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-02-25 19:40:41 +0100

    file handling

---
+++ File_Handling/Creating_a_file.py
@@ -0,0 +1,9 @@
+file_to_be_created = open("pyCharm.txt", "w")
+file_to_be_created.write("This is my first file creation")
+file_to_be_created.write(" It will appear on my file list")
+file_to_be_created.close()
+
+
+creating_an_account_of_some_students = open("Students account.txt", "w")
+creating_an_account_of_some_students.write("")
+creating_an_account_of_some_students.close()
---
+++ File_Handling/importing_a_file_from_another_module.py
@@ -0,0 +1,17 @@
+# import Creating_a_file
+#
+# Creating_a_file
+#
+# file_to_be_created = open("pyCharm.txt", "a")
+# file_to_be_created.write("\n\rThis is the appending part of this code." )
+
+
+from decimal import Decimal
+import Creating_a_file
+
+Creating_a_file
+
+This_is_a_student_account = open("Students account.txt")
+value = Decimal('12.34')
+
+student_first_account1 = This_is_a_student_account
